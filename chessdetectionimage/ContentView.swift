f34c627a804f3fb540ef53deb7448dcc89dbefb9
ygthnzdmr <149046580+ygthnzdmr@users.noreply.github.com>
2026-01-07 16:48:27 +0300

    Initial Commit

---
+++ chessdetectionimage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chessdetectionimage
+//
+//  Created by Yiğithan Özdemir on 7.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chessdetectionimage/chessdetectionimageApp.swift
@@ -0,0 +1,17 @@
+//
+//  chessdetectionimageApp.swift
+//  chessdetectionimage
+//
+//  Created by Yiğithan Özdemir on 7.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct chessdetectionimageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
